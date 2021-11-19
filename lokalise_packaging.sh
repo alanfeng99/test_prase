d05b35a9ec390f9e2248bdf3cb8bf6624ffd072c
alanfeng99 <alan.kl.feng@gmail.com>
2021-11-19 17:54:01 +0800

    add sh

---
+++ lokalise_packaging.sh
@@ -0,0 +1,8 @@
+NOW=$(date +"%Y-%m-%d %H-%M")
+curl --location --request POST 'https://api.lokalise.com/api2/projects/39238065617a2548df77e0.74612171/files/download' \
+--header 'X-Api-Token: e658433f29d39ae731b7f51a23f1a20425e7a039' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "format": "ios_sdk",
+    "bundle_description": "$NOW"
+}'
