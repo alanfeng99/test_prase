60311d1294aac1fe79af07894c5b01f6bb00056f
alanfeng99 <alan.kl.feng@gmail.com>
2021-11-19 17:56:26 +0800

    update sh

--- lokalise_packaging.sh
+++ lokalise_packaging.sh
@@ -4,5 +4,5 @@ curl --location --request POST 'https://api.lokalise.com/api2/projects/392380656
 --header 'Content-Type: application/json' \
 --data-raw '{
     "format": "ios_sdk",
-    "bundle_description": '.$NOW.'
+    "bundle_description": "'.$NOW.'"
 }'
