9d7e81fc3db1648fe7164d15563d2dfe23915a80
alanfeng99 <alan.kl.feng@gmail.com>
2021-11-19 18:35:11 +0800

    test

--- lokalise_packaging.sh
+++ lokalise_packaging.sh
@@ -1,7 +1,10 @@
 NOW="$(date +"%Y-%m-%d %H-%M")"
 API_TOKEN="e658433f29d39ae731b7f51a23f1a20425e7a039"
-BODY='{"format": "ios_sdk","bundle_description": "'$NOW'"}'
+PLATFORM="ios_sdk"
 curl --location --request POST 'https://api.lokalise.com/api2/projects/39238065617a2548df77e0.74612171/files/download' \
 --header 'X-Api-Token: '$API_TOKEN'' \
 --header 'Content-Type: application/json' \
---data-raw ''$BODY''
+--data-raw '{
+    "format": "'$PLATFORM'",
+    "bundle_description": "aaa"
+}'
